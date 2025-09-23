50c56e8f78ed9f6f7bd18388f6c16ca1147da9ab
promotion-pal <promotion.work.pal@gmail.com>
2025-09-24 09:57:12 +1000

    test traefik

--- fesmu-admin/src/core/dataProvider.tsx
+++ fesmu-admin/src/core/dataProvider.tsx
@@ -5,8 +5,8 @@ import {
   fetchUtils,
 } from "react-admin";
 
-// const apiUrl = "http://api.fesmu.localhost";
-const apiUrl = process.env.REACT_APP_API;
+const apiUrl = "http://api.fesmu.promotion-pal.ru";
+// const apiUrl = process.env.REACT_APP_API;
 const httpClient = fetchUtils.fetchJson;
 
 export const dataProvider = {
