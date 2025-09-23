8830acf763130ad411869eb040d6a8bce34b2f6e
promotion-pal <promotion.work.pal@gmail.com>
2025-09-24 09:53:32 +1000

    test traefik

--- fesmu-admin/vite.config.ts
+++ fesmu-admin/vite.config.ts
@@ -1,14 +1,44 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   plugins: [react()],
+//   server: {
+//     host: true,
+//   },
+//   build: {
+//     sourcemap: mode === "development",
+//   },
+//   base: "./",
+// }));
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: "0.0.0.0",
+    port: 4173,
+    allowedHosts: [
+      "admin.fesmu.promotion-pal.ru",
+      "fesmu.promotion-pal.ru",
+      "api.fesmu.promotion-pal.ru",
+    ],
+    https: false,
+    cors: true,
+    proxy: {},
+  },
   server: {
-    host: true,
+    host: "0.0.0.0",
+    port: 4173,
+    allowedHosts: true,
+    cors: true,
   },
+  // Для production билда
   build: {
-    sourcemap: mode === "development",
+    outDir: "dist",
+    sourcemap: false,
   },
-  base: "./",
-}));
+});
