eee3ecc32f7c9773070fe58ff5eb37c30bc35092
promotion-pal <promotion.work.pal@gmail.com>
2026-01-21 15:52:46 +1000

    test

--- fesmu-web/src/app/board/page.tsx
+++ fesmu-web/src/app/board/page.tsx
@@ -1,51 +1,55 @@
-"use client";
-
-import { PromButton, PromFrom, PromInput } from "prom-pal-ui";
-import z from "zod";
-
-export default function BoardPage() {
-  const handle = async () => {
-    try {
-      const res = await fetch("/kk");
-
-      if (!res.ok) {
-        throw new Error(JSON.stringify({ error: "олдоалво" }));
-      }
-
-      console.log(res);
-    } catch (error) {
-      console.log("!!!!!");
-      throw error;
-    }
-  };
-
-  const schema = z.object({
-    text: z
-      .string()
-      .min(2, { message: "Трахну тебя в сраку, пиши более 2 букв пидор" }),
-  });
-
-  return (
-    <section>
-      <PromFrom
-        defaultValues={{
-          text: "",
-        }}
-        schema={schema}
-        onSubmit={handle}
-        render={({ serverError }) => (
-          <section className="container space-y-4">
-            <PromInput label="ЖОпа" name="text" />
-            <PromButton>fdsfd</PromButton>
-
-            {serverError && (
-              <pre className="mt-2 rounded-lg bg-red-200 px-3 py-1 text-red-500">
-                {JSON.parse(serverError).error}
-              </pre>
-            )}
-          </section>
-        )}
-      />
-    </section>
-  );
+export default function page() {
+  return <div>page</div>;
 }
+
+// "use client";
+
+// import { PromButton, PromFrom, PromInput } from "prom-pal-ui";
+// import z from "zod";
+
+// export default function BoardPage() {
+//   const handle = async () => {
+//     try {
+//       const res = await fetch("/kk");
+
+//       if (!res.ok) {
+//         throw new Error(JSON.stringify({ error: "олдоалво" }));
+//       }
+
+//       console.log(res);
+//     } catch (error) {
+//       console.log("!!!!!");
+//       throw error;
+//     }
+//   };
+
+//   const schema = z.object({
+//     text: z
+//       .string()
+//       .min(2, { message: "Трахну тебя в сраку, пиши более 2 букв пидор" }),
+//   });
+
+//   return (
+//     <section>
+//       <PromFrom
+//         defaultValues={{
+//           text: "",
+//         }}
+//         schema={schema}
+//         onSubmit={handle}
+//         render={({ serverError }) => (
+//           <section className="container space-y-4">
+//             <PromInput label="ЖОпа" name="text" />
+//             <PromButton>fdsfd</PromButton>
+
+//             {serverError && (
+//               <pre className="mt-2 rounded-lg bg-red-200 px-3 py-1 text-red-500">
+//                 {JSON.parse(serverError).error}
+//               </pre>
+//             )}
+//           </section>
+//         )}
+//       />
+//     </section>
+//   );
+// }
